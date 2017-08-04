1897334120997f451198f0939c8c6ca46e5098e5
Zach Jones <prozachj@gmail.com>
2017-08-03 22:05:12 -0500

    good boilerplate spot

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
 'use strict'
+const $ = require('jquery')
 const Backbone = require('backbone')
+const Mustache = require('mustache')
 
 const Hello = Backbone.View.extend({
   initialize: function () {
     this.render()
   },
   render: function () {
-    this.$el.append('<h1>Here we have rendered a backbone view.</h1>')
+    const data = { b: 'backbone', m: 'mustache' }
+    this.$el.append(Mustache.render('<h1>Here we have rendered a {{m}} template in a {{b}} view.</h1>', data))
   }
 })
 
-new Hello({el: document.querySelector('.container')})
+new Hello({el: $('.container')})
